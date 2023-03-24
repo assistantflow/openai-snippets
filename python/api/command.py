175ab580ecfd4e35d08545aba97dca370d3c0194
douglarek <douglarek@gmail.com>
2023-03-25 01:07:54 +0800

    Use openai SDK

--- python/api/command.py
+++ python/api/command.py
@@ -1,90 +1,64 @@
+import openai
 import decorators
-import aiohttp
 import asyncio
 import os
-import requests
-import json
 import sys
 import click
 
 
 @click.group()
 @click.option('--base_url', required=False, default='https://api.openai.com')
-@click.pass_context
-def openai(ctx, base_url):
-    ctx.ensure_object(dict)
-    ctx.obj['URL'] = base_url
+def main(base_url):
+    openai.api_base = base_url + "/v1"
     key = os.getenv('OPENAI_API_KEY')
     if not key:
         print('no key env set, exit ...')
         sys.exit(-1)
-    ctx.obj['KEY'] = key
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@openai.command()
+@main.command()
 @click.argument('content', required=True)
 @click.option('--stream', required=False, default=True, type=bool)
-@click.pass_context
 @decorators.coro
-async def chat(ctx, content, stream):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(ctx.obj['URL'] + '/v1/chat/completions',
-                                headers={'Authorization': 'Bearer {}'.format(ctx.obj['KEY'])},
-                                json={
-                                    'stream': stream,
-                                    "model": "gpt-3.5-turbo",
-                                    "messages": [{"role": "user", "content": content}]
-        }) as resp:
-            if resp.status != 200:
-                s = await resp.text()
-                print(s)
-                return
-
-            async for line in resp.content:
-                v = line[len('data: '):]
-                if v and not v.startswith(b"[DONE]"):
-                    s = json.loads(v)
-                    if 'choices' in s:
-                        c = s['choices'][0]['delta'].get('content', '')
-                        sys.stdout.write(c)
-                        sys.stdout.flush()
-            print()
+async def chat(content, stream):
+    completions = await openai.ChatCompletion.acreate(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "user", "content": content}],
+        stream=stream
+    )
+    if stream:
+        async for c in completions:
+            sys.stdout.write(c.choices[0].delta.get('content', ''))
+            sys.stdout.flush()
+    else:
+        print(completions.choices[0].message.content)
+    print()
 
 
-@openai.command
+@main.command
 @click.argument('prompt', required=True)
 @click.option('--max_tokens', required=False, default=1024, show_default=True)
 @click.option('--temperature', required=False, default=0.5, show_default=True)
 @click.option('--stream', required=False, default=True, type=bool)
-@click.pass_context
 @decorators.coro
-async def complete(ctx, prompt, max_tokens, temperature, stream):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(ctx.obj['URL'] + '/v1/completions',
-                                headers={'Authorization': 'Bearer {}'.format(ctx.obj['KEY'])},
-                                json={
-                                    'stream': stream,
-                                    'model': 'text-davinci-003',
-                                    'prompt': prompt,
-                                    'max_tokens': max_tokens,
-                                    'temperature': temperature}
-                                ) as resp:
-            if resp.status != 200:
-                s = await resp.text()
-                print(s)
-                return
-
-            async for line in resp.content:
-                v = line[len('data: '):]
-                if v and not v.startswith(b"[DONE]"):
-                    s = json.loads(v)
-                    if 'choices' in s:
-                        c = s['choices'][0].get('text', '')
-                        sys.stdout.write(c)
-                        sys.stdout.flush()
-            print()
+async def complete(prompt, max_tokens, temperature, stream):
+    completions = await openai.Completion.acreate(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        stream=stream
+    )
+    if stream:
+        async for c in completions:
+            sys.stdout.write(c.choices[0].text)
+            sys.stdout.flush()
+    else:
+        print(completions.choices[0].text)
+    print()
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(openai())
+    loop.run_until_complete(main())
