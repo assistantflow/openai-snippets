2c2761f7d3deb72be03a9571e5c27060eac03c19
douglarek <douglarek@gmail.com>
2023-03-25 00:31:58 +0800

    Added completions and asyncio

--- python/api/command.py
+++ python/api/command.py
@@ -1,3 +1,6 @@
+import decorators
+import aiohttp
+import asyncio
 import os
 import requests
 import json
@@ -22,25 +25,66 @@ def openai(ctx, base_url):
 @click.argument('content', required=True)
 @click.option('--stream', required=False, default=True, type=bool)
 @click.pass_context
-def chat(ctx, content, stream):
-    with requests.Session().post(
-            ctx.obj['URL']+'/v1/chat/completions',
-            headers={'Authorization': 'Bearer {}'.format(ctx.obj['KEY'])},
-            stream=stream,
-            json={
-                "model": "gpt-3.5-turbo",
-                "stream": stream,
-                "messages": [{"role": "user", "content": content}],
-            }) as resp:
-        for line in resp.iter_lines():
-            v = line[len('data: '):]
-            if v and v != b"[DONE]":
-                s = json.loads(v)
-                if 'choices' in s:
-                    c = s['choices'][0]['delta'].get('content', '')
-                    sys.stdout.write(c)
-                    sys.stdout.flush()
+@decorators.coro
+async def chat(ctx, content, stream):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(ctx.obj['URL'] + '/v1/chat/completions',
+                                headers={'Authorization': 'Bearer {}'.format(ctx.obj['KEY'])},
+                                json={
+                                    'stream': stream,
+                                    "model": "gpt-3.5-turbo",
+                                    "messages": [{"role": "user", "content": content}]
+        }) as resp:
+            if resp.status != 200:
+                s = await resp.text()
+                print(s)
+                return
+
+            async for line in resp.content:
+                v = line[len('data: '):]
+                if v and not v.startswith(b"[DONE]"):
+                    s = json.loads(v)
+                    if 'choices' in s:
+                        c = s['choices'][0]['delta'].get('content', '')
+                        sys.stdout.write(c)
+                        sys.stdout.flush()
+            print()
+
+
+@openai.command
+@click.argument('prompt', required=True)
+@click.option('--max_tokens', required=False, default=1024, show_default=True)
+@click.option('--temperature', required=False, default=0.5, show_default=True)
+@click.option('--stream', required=False, default=True, type=bool)
+@click.pass_context
+@decorators.coro
+async def complete(ctx, prompt, max_tokens, temperature, stream):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(ctx.obj['URL'] + '/v1/completions',
+                                headers={'Authorization': 'Bearer {}'.format(ctx.obj['KEY'])},
+                                json={
+                                    'stream': stream,
+                                    'model': 'text-davinci-003',
+                                    'prompt': prompt,
+                                    'max_tokens': max_tokens,
+                                    'temperature': temperature}
+                                ) as resp:
+            if resp.status != 200:
+                s = await resp.text()
+                print(s)
+                return
+
+            async for line in resp.content:
+                v = line[len('data: '):]
+                if v and not v.startswith(b"[DONE]"):
+                    s = json.loads(v)
+                    if 'choices' in s:
+                        c = s['choices'][0].get('text', '')
+                        sys.stdout.write(c)
+                        sys.stdout.flush()
+            print()
 
 
 if __name__ == '__main__':
-    openai()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(openai())
